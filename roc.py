55fa9fbbc9d7ac019c9bf51768dab720ffc0e5c3
0x00-pl <0x00.pl@gmail.com>
2019-02-09 07:45:08 +0000

    add code

--- roc.py
+++ roc.py
@@ -1,5 +1,3 @@
-
-
 def roc_from_dist_tag_tag(dist_tag_tag_list, limit=None):
     data = sorted(dist_tag_tag_list, key=(lambda x: x[0]))
     true_sum = sum(
@@ -17,7 +15,15 @@ def roc_from_dist_tag_tag(dist_tag_tag_list, limit=None):
         false_pos = pos_count - true_pos
         true_neg = true_sum - true_pos
         false_neg = total_sum - true_sum - false_pos
-        if limit is None or int(round(pos_count / total_sum * limit)) != int(round((pos_count - 1) / total_sum * limit)):
+        if limit is None or int(round(pos_count / total_sum * limit)) != int(
+                round((pos_count - 1) / total_sum * limit)):
             ret.append([dist, true_neg, false_pos])
 
     return ret
+
+
+def print_roc(roc_list, name='roc'):
+    print(name, '=', end='[')
+    for trueneg, falsepos, threahold in roc_list:
+        print(trueneg, falsepos, threahold, end=';')
+    print('];')
